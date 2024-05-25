90f8548e3219f6991f7ac079119a4b93d1a8f14f
gabrielorod <gabrieloliveirarodrigues8@hotmail.com>
2024-05-25 07:53:35 -0300

    feat: jwt configure

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from "@nestjs/config";
 import { PrismaService } from "./prisma/prisma.service";
 import { CreateUserController } from "./controllers/create-user.controller";
 import { envSchema } from "./env";
+import { AuthModule } from "./auth/auth.modules";
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { envSchema } from "./env";
       validate: (env) => envSchema.parse(env),
       isGlobal: true,
     }),
+    AuthModule,
   ],
   controllers: [CreateUserController],
   providers: [PrismaService],

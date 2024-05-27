c65dfc9297cf4b48f79519cd31add3bf3098ccd3
gabrielorod <gabrieloliveirarodrigues8@hotmail.com>
2024-05-27 05:32:49 -0300

    refactor: get user by id

--- src/http/users/get-user-by-id.controller.ts
+++ src/http/users/get-user-by-id.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Param } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
 
 @Controller('/v1/user')
@@ -7,8 +7,40 @@ export class GetUserByIdController {
 
   @Get(':id')
   async userById(@Param('id') id: string) {
-    return this.prisma.user.findUnique({
+    const user = await this.prisma.user.findUnique({
       where: { id: id },
     });
+
+    if (!user) {
+      throw new BadRequestException('Invalid field.');
+    }
+
+    const group = await this.prisma.group.findUnique({
+      where: {
+        id: user.id_group,
+      },
+    });
+
+    const bankHours = await this.prisma.bankHour.findMany({
+      where: {
+        id_user: user.id,
+      },
+    });
+
+    const totalBankHours = bankHours.reduce((acc, current) => acc + 1, 0); // Lugar do 1 adicionar current.hour
+
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      team: user.team,
+      hourValue: user.hourValue,
+      hasBankHours: user.hasBankHours,
+      totalBankHours: totalBankHours,
+      contractTotal: user.contractTotal,
+      groupId: group?.id,
+      groupName: group?.name,
+      startDate: user.startDate,
+    };
   }
 }
